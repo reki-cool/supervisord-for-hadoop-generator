05da1d4b0c13b5bb1ea41ca1f291e222c4bd4bea
reki-cn <2504621508@qq.com>
2019-12-05 10:05:48 +0800

    update

--- src/main/java/com/duyanhan/Supervisor4DaemonConfigurationGeneratorApplication.java
+++ src/main/java/com/duyanhan/Supervisor4DaemonConfigurationGeneratorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroServiceCodeGeneratorApplication {
+public class Supervisor4DaemonConfigurationGeneratorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroServiceCodeGeneratorApplication.class, args);
+        SpringApplication.run(Supervisor4DaemonConfigurationGeneratorApplication.class, args);
     }
 
 }
--- src/test/java/com/duyanhan/Supervisor4DaemonConfigurationGeneratorApplicationTests.java
+++ src/test/java/com/duyanhan/Supervisor4DaemonConfigurationGeneratorApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MicroServiceCodeGeneratorApplicationTests {
+class Supervisor4DaemonConfigurationGeneratorApplicationTests {
 
     @Test
     void contextLoads() {
